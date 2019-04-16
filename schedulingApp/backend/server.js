976d1953586f63fd4a8f18010cfdb86aa1b27c21
Gerson Cruz <gcruz1776@gmail.com>
2019-04-16 01:22:35 -0500

    initial commmit

---
+++ schedulingApp/backend/server.js
@@ -0,0 +1,15 @@
+//dependencies to run app
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 3000;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//port that the server is running on
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
