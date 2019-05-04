99f2c8fc8b666ae38a7e22f8461439629da4899b
Gerson Cruz <gcruz1776@gmail.com>
2019-05-04 14:02:16 -0500

    create form component and pages

---
+++ client/src/components/Login/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render() {
+        return (
+            <div>Hi!</div>
+)
+    }
+}
+
+export default Form;
--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,3 +1,13 @@
 // Initial Log in and landing page
 // user name and password 
 //https://medium.com/@dternyak/react-redux-flask-a-modern-web-application-boilerplate-fce1bfa62594
+import React from 'react';
+import Form from '../components/Login/Form'
+
+const Login = () => {
+    return (
+        <Form/>
+    )
+}
+
+export default Login;
