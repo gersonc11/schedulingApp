e0078950351670c73ccdf73a087bea88d6dfb34d
Gerson Cruz <gcruz1776@gmail.com>
2019-05-07 21:21:12 -0500

    created employee mongoose schema

--- models/employees.js
+++ models/employees.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+const employeeSchema = new Schema({
   name: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true},
   wage: { type: Number, required: true }
 });
 
-const Employee = mongoose.model("Employee", bookSchema);
+const Employee = mongoose.model("Employee", employeeSchema);
 
 module.exports = Employee;
